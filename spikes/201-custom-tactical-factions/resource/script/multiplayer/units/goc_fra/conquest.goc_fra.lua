e411244cfabe0c0be7575b5f9e42351950ab65db
Paul Fiesel <87280532+xfizzle-gh@users.noreply.github.com>
2026-08-10 22:47:51 -0500

    fix(spike): strip UTF-8 BOM from set/inc overlays

--- spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_fra/conquest.goc_fra.lua
+++ spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_fra/conquest.goc_fra.lua
@@ -1,4 +1,4 @@
-﻿Purchases = Purchases or {}
+Purchases = Purchases or {}
 Purchases["conquest.goc_fra"] = {
   {repeat = 1, units = {{unit = "goc_fra_test_rifle(goc_fra)", unit_count = 2}}},
   {repeat = 1, units = {{unit = "goc_fra_test_at(goc_fra)", unit_count = 1}}},
--- spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_usa/conquest.goc_usa.lua
+++ spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_usa/conquest.goc_usa.lua
@@ -1,4 +1,4 @@
-﻿Purchases = Purchases or {}
+Purchases = Purchases or {}
 Purchases["conquest.goc_usa"] = {
   {repeat = 1, units = {{unit = "goc_usa_test_rifle(goc_usa)", unit_count = 2}}},
   {repeat = 1, units = {{unit = "goc_usa_test_at(goc_usa)", unit_count = 1}}},
