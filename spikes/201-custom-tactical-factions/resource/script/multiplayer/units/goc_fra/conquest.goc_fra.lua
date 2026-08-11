fd80756d110a200760f30af611728d7f6a0271d8
xFizzle <87280532+xfizzle-gh@users.noreply.github.com>
2026-08-11 00:46:48 -0500

    fix #201 purchase script schema

--- spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_fra/conquest.goc_fra.lua
+++ spikes/201-custom-tactical-factions/resource/script/multiplayer/units/goc_fra/conquest.goc_fra.lua
@@ -1,6 +1,10 @@
 Purchases = Purchases or {}
 Purchases["conquest.goc_fra"] = {
-  {repeat = 1, units = {{unit = "goc_fra_test_rifle(goc_fra)", unit_count = 2}}},
-  {repeat = 1, units = {{unit = "goc_fra_test_at(goc_fra)", unit_count = 1}}},
-  {repeat = 1, units = {{unit = "goc_fra_test_vehicle(goc_fra)", unit_count = 1}}},
+	{Repeat = 0,
+		Units = {
+			{priority = 2.2, type = {"Class2", "Infantry", "Squad"}, unit = "goc_fra_test_rifle(goc_fra)"},
+			{priority = 1.5, type = {"Class2", "Infantry", "Squad", "AT"}, unit = "goc_fra_test_at(goc_fra)"},
+			{priority = 0.8, type = {"Class1", "Armored", "Vehicle"}, unit = "goc_fra_test_vehicle(goc_fra)"},
+		}
+	},
 }
